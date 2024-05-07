667b6d169fb9c38901e6802f4d96fd5cafb3a7f5
Naod Taddese <naodtds@gmail.com>
2024-05-07 21:02:52 +0300

    cors

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 // third party modules
 const express = require("express");
+const cors = require("cors")
 const { cloudinaryConfig } = require("./utilities/cloudinary-config");
 const app = express();
 const news_routes = require("./routes/news");
+const corsOptions = {
+  //origin: 'https://sudo-delivery-frontend.vercel.app',
+  exposedHeaders: "*",
+};
+
+app.use(cors(corsOptions));
+
+
 app.get("/chek", (req, res) => {
   res.send("Hello World");
 });
--- controllers/news.js
+++ controllers/news.js
@@ -2,6 +2,7 @@ const { NewsScreenshot } = require("../models/news-screenshot");
 
 exports.getNews = async (req, res) => {
   const { date } = req.query;
+  console.log(req.query);
   const news = await NewsScreenshot.find({ dateOfScreenshot: date });
   if (!news) return res.status(404).send("No news found for the given date");
   res.json(news);
